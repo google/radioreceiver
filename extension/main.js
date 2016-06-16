756b5237f190b83e49558d6b3d5e29135a3baeb9
Jacobo Tarrio <jtarrio@gmail.com>
2016-06-16 09:43:51 -0700

    Remove unused variable.

--- extension/main.js
+++ extension/main.js
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-var usedConnections = [];
-
 chrome.app.runtime.onLaunched.addListener(function() {
   chrome.app.window.create('interface.html', {
     'id': 'radioTuner',
